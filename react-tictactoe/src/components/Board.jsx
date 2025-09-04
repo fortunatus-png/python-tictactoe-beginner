6fbee6b667c9b08c0350f39e2cbf81abc1464246
Tumar Chegebaeva <tumar.chegebaeva@informatik.hs-fulda.de>
2025-09-04 16:14:22 +0200

    Implement full gameplay (winner, draw, restart)

--- react-tictactoe/src/components/Board.jsx
+++ react-tictactoe/src/components/Board.jsx
@@ -2,12 +2,10 @@
 const Board = ({ board }) => {
 
     return (
-        <div className='buttons'>
-            <h2>Game ongoing</h2>
-
-            <div className="fstRow row"> {board[0]} {board[1]} {board[2]} </div>
-            <div className="sndRow row"> {board[3]} {board[4]} {board[5]} </div>
-            <div className="thirdRow row"> {board[6]} {board[7]} {board[8]} </div>
+        <div className="buttons">
+            <div className="row"> {board[0]}{board[1]}{board[2]} </div>
+            <div className="row"> {board[3]}{board[4]}{board[5]} </div>
+            <div className="row"> {board[6]}{board[7]}{board[8]} </div>
         </div>
     );
 }
--- react-tictactoe/src/components/Game.jsx
+++ react-tictactoe/src/components/Game.jsx
@@ -1,45 +1,60 @@
 import { useState } from 'react';
-import Board from './Board'
+import Board from './Board';
+import CheckWinner from './CheckWinner';
 
 const Game = () => {
 
     let initialBoard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
     const [board, setBoard] = useState(initialBoard);
     const [turn, setTurn] = useState(0);
-    /*  const [res,setRes]=useState(false);
- 
-     function checkWinner() {
- 
-     } */
+    const [winner, setWinner] = useState(null);
+
+    let currentPlayer = turn % 2 === 0 ? 'X' : 'O';
 
     function handleXO(index) {
+        if (board[index] !== ' ' || winner) return;
 
-        let currentPlayer = ' ';
-        if (turn % 2 === 0) {
-            currentPlayer = 'X';
-        } else {
-            currentPlayer = 'O';
+        const newBoard = board.slice();
+        newBoard[index] = currentPlayer;
+        setBoard(newBoard);
+        setTurn(turn + 1);
+
+        const result = CheckWinner(newBoard);
+        if (result) {
+            setWinner(result);
+            return;
         }
+    }
 
-        const currentBoard = board.map((c, i) => {
-            if (i === index && c === ' ') {
-                setTurn(turn + 1);
-                return c = currentPlayer;
-            } else {
-                return c;
-            }
-        });
-        setBoard(currentBoard);
+    function restart() {
+        setBoard(initialBoard);
+        setTurn(0);
+        setWinner(null);
     }
 
     return (
-        <>
-            <Board board={board.map((cell, i) => (
-                <button key={i} onClick={() => { handleXO(i); }}>{cell}</button>
-            ))} />
-        </>
-    );
+        <div className='container'>
 
+            <div className='first'>
+                <h2>
+                    {
+                        winner ? `Player ${winner} wins!` : turn === 9
+                            ? "It's a draw!" : `Next player ${currentPlayer}`
+                    }
+                </h2>
+            </div>
+
+            <div className='second'>
+                <Board board={board.map((cell, i) => (
+                    <button className='btn' key={i} onClick={() => { handleXO(i); }}>{cell}</button>
+                ))} />
+            </div>
+
+            <div className='third'>
+                <button className='res' onClick={restart}>Restart Game</button>
+            </div>
+        </div>
+    );
 }
 
 export default Game;
